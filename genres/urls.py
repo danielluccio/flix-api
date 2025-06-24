4da827eafab0d1c562f4b8f4a8fee2d31e0338d4
danielluccio <daniel130877@gmail.com>
2025-06-23 22:38:17 -0300

    CRUD sem drf completo e funcionando

--- genres/urls.py
+++ genres/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_genres
+from .views import genre_create_list_view, genre_detail_view
 
 urlpatterns = [
-    path('', list_genres, name='list_genres'),
+    path('', genre_create_list_view, name='list_genres'),
+    path('<int:pk>', genre_detail_view, name='genre_detail_view')
 ]
